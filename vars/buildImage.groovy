5c9e5f295da6915b0b363826393c396e136b0fd9
akram-cr <akram.cheikhrouhou2013@gmail.com>
2025-12-09 19:50:03 +0100

    initial commit

---
+++ vars/buildImage.groovy
@@ -0,0 +1,10 @@
+#!/user/bin/env groovy
+
+def call() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t crakram/test-repo:jma-2.0 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push crakram/test-repo:jma-2.0'
+    }
+}
