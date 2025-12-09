0628b0f83893d41d6de60b570432c5b016297656
akram-cr <akram.cheikhrouhou2013@gmail.com>
2025-12-09 20:34:22 +0100

    add scripts to src groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,12 +1,8 @@
 #!/user/bin/env groovy
 
-def call(String imageName) {
-    echo "building the docker image from branch name $BRANCH_NAME"
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        // sh 'docker build -t crakram/test-repo:jma-2.0 .'
-        sh "docker build -t $imageName ."
 
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh "docker push $imageName"
-    }
+import com.example.Docker
+
+def call(String imageName) {
+    return new Docker(this).buildDockerImage(imageName)
 }
