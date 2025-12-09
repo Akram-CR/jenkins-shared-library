c61962429d8ecc935a5ca69fc21ab01f3235cbba
akram-cr <akram.cheikhrouhou2013@gmail.com>
2025-12-09 20:11:25 +0100

    call docker bbuild with params

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,12 @@
 #!/user/bin/env groovy
 
-def call() {
-    echo "building the docker image..."
+def call(String imageName) {
+    echo "building the docker image from branch name $BRANCH_NAME"
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t crakram/test-repo:jma-2.0 .'
+        // sh 'docker build -t crakram/test-repo:jma-2.0 .'
+        sh "docker build -t $imageName ."
+
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push crakram/test-repo:jma-2.0'
+        sh "docker push $imageName"
     }
 }
