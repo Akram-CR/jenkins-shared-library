d80d453315866de69596a79ebae86224514c596d
akram-cr <akram.cheikhrouhou2013@gmail.com>
2025-12-09 21:04:52 +0100

    seperate docker commands over files

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -10,13 +10,16 @@ class Docker implements  Serializable {
 
     def buildDockerImage(String imageName) {
         script.echo "building the docker image from branch name ${script.env.BRANCH_NAME}"
+        script.sh "docker build -t $imageName ."
+    }
+
+    def dockerLogin() {
         script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-            // sh 'docker build -t crakram/test-repo:jma-2.0 .'
-            script.sh "docker build -t $imageName ."
-
             script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
-            script.sh "docker push $imageName"
         }
+    }
 
+    def dockerPush(String imageName) {
+        script.sh "docker push $imageName"
     }
 }
